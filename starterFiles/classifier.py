49674f77f1dc7343a6bcc7194ba40e35bea5d7ab
Piotr Brudny <pbrudny@gmail.com>
2024-04-02 02:32:58 +0200

    Final touches

--- starterFiles/classifier.py
+++ starterFiles/classifier.py
@@ -3,6 +3,19 @@ from tensorflow.keras import models
 from PIL import Image
 import numpy as np
 
+class_names = {
+    0: 'airplane',
+    1: 'automobile',
+    2: 'bird',
+    3: 'cat',
+    4: 'deer',
+    5: 'dog',
+    6: 'frog',
+    7: 'horse',
+    8: 'ship',
+    9: 'truck',
+}
+
 model = models.load_model("baseline_mariya.keras")
 
 def predict_image(model, path_to_img):
@@ -10,13 +23,18 @@ def predict_image(model, path_to_img):
     img = img.convert("RGB")
     img = img.resize((32, 32))
     data = np.asarray(img)
-    print("before", data[0][0])
     data = data / 255
-    print("after", data[0][0])
+    probs = model.predict(np.array([data][:1]))
+    top_prob = probs.max()
+    top_pred = class_names[np.argmax(probs)]
+
+    return top_prob, top_pred
 
 
 content = ""
 img_path = "placeholder_image.png"
+prob = 0
+pred = ""
 
 index = """
 <|text-center|
@@ -25,16 +43,20 @@ index = """
 <|{content}|file_selector|extensions=.png|>
 select an image from your file system
 
+<|{pred}|>
+
 <|{img_path}|image|>
 
-<|label goes here|indicator|value=0|min=0|max=100|width=25vw|>
+<|{prob}|indicator|value={prob}|min=0|max=100|width=25vw|>
 >
 """
 
 def on_change(state, var_name, var_val):
     if var_name == "content":
+        top_prob, top_pred = predict_image(model, var_val)
+        state.prob = round(top_prob * 100)
+        state.pred = "this is a " + top_pred
         state.img_path = var_val
-        predict_image(model, var_val)
     # print(var_name, var_val)
 
 
